caed4411297a93d32c335bc03178de5ae857c709
Lukasz Szafron <lukszafron@gmail.com>
2023-08-07 21:39:02 +0200

    Update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# vep.r
-
-This program requires nine arguments. The first one is the destination folder, the second one is a semicolon-delimited CSV file with sample names and grouping variables, the third determines the sample ID variable, the fourth - grouping variable, the fifth - independent factor, the sixth - file with a gene list (one gene per line) to calculate the gene signature, the seventh is the number of threads to use, the eighth says if samples are paired, while the ninth determines the column with pair indicators.
+This program requires ten arguments.
+                                  The first one is the destination folder,
+                                  the second one is a semicolon-delimited CSV file with sample names and grouping variables,
+                                  the third determines the sample ID variable,
+                                  the fourth - grouping variable,
+                                  the fifth - independent factor,
+                                  the sixth - file with a gene list (one gene per line) to calculate the gene signature,
+                                  the seventh is the number of threads to use,
+                                  the eighth says if samples are paired,
+                                  the ninth determines the column with pair indicators,
+                                  while the tenth is a logical indicator determining if the FDR correction should be applied in the TOST analysis of equivalence.
--- vep.r
+++ vep.r
@@ -3,7 +3,17 @@ cat("Program path:", unlist(strsplit(grep(commandArgs(), pattern = "file=", valu
 
 arguments <- commandArgs(trailingOnly = TRUE)
 
-if(length(arguments) != 10) {stop("This program requires nine arguments. The first one is the destination folder, the second one is a semicolon-delimited CSV file with sample names and grouping variables, the third determines the sample ID variable, the fourth - grouping variable, the fifth - independent factor, the sixth - file with a gene list (one gene per line) to calculate the gene signature, the seventh is the number of threads to use, the eighth says if samples are paired, the ninth determines the column with pair indicators, while the tenth is a logical indicator determining if the FDR correction should be aplied in the TOST analysis of equivalence.")}
+if(length(arguments) != 10) {stop("This program requires ten arguments.
+                                  The first one is the destination folder,
+                                  the second one is a semicolon-delimited CSV file with sample names and grouping variables,
+                                  the third determines the sample ID variable,
+                                  the fourth - grouping variable,
+                                  the fifth - independent factor,
+                                  the sixth - file with a gene list (one gene per line) to calculate the gene signature,
+                                  the seventh is the number of threads to use,
+                                  the eighth says if samples are paired,
+                                  the ninth determines the column with pair indicators,
+                                  while the tenth is a logical indicator determining if the FDR correction should be applied in the TOST analysis of equivalence.")}
 arguments
 arguments.backup <- arguments
 
